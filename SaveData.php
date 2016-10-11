944d5b9e88784e63b7b23b9580f70980a469895f
Silvan Herrema <silvah489@gmail.com>
2016-10-11 15:26:51 +0200

    cleaned some code and added comments

--- SaveData.php
+++ SaveData.php
@@ -1,14 +1,23 @@
 <?php
   $score=$_REQUEST['score'];
-
+  $server = 'localhost';
+  $username = 'Student';
+  $password = 'NicCage';
+  $database = 'hers_myRunner';
   //sanitize input
 
+  //check if score was empty. Remember to put ?score=### after your url to insert a score
   if($score != ""){
-    $database = mysqli_connect('localhost', 'Student', 'NicCage', 'hers_myRunner') or die('Failed to connect: ' . mysql_error());
-    $timeStamp = date('Y-m-d H:i:s');
+    //make connection with the database
+    $database = mysqli_connect($server, $username, $password, $database) or die('Failed to connect: ' . mysql_error());
+
+    //create the SQL statement used to insert data into the database
+    //use your own table name, value names and values.
     $query="INSERT INTO HighScores(ID, Score) VALUES(DEFAULT, '$score');";
+    //Run the SQL query
     $result=mysqli_query($query);
-    echo "$score, $playerID, $timeStamp success?";
+    //If no error occured this gets printed. If nothing happens in the database, this means that the SQL query wasn't executed.
+    echo "$result success?";
   } else{
     echo "Bro score was empty bro";
   }
