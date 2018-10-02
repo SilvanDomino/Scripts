87d0e2a8621e18de4fd17aac068ec4f2f1baf462
Silvan Herrema <>
2018-10-02 14:46:44 +0200

    Jump Scripts added

---
+++ GD2 Jump/InputManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour {
+
+    public bool Jump(){
+        return Input.GetButton("Jump");
+    }
+    public bool JumpDown()
+    {
+        return Input.GetButtonDown("Jump");
+    }
+}
