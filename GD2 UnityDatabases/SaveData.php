c26bea520f2b644b0ef92c8ae714057aacd55bad
Silvan Herrema <silvah489@gmail.com>
2019-10-17 10:03:18 +0200

    Update SaveData.php

--- GD2 UnityDatabases/SaveData.php
+++ GD2 UnityDatabases/SaveData.php
@@ -1,29 +1,24 @@
 <?php
+  //add ?score=5 to the url
   $score=$_REQUEST['score'];
-
-  $host = 'localhost';
+  $host = 'localhost:8889';
   $user = 'root';
-  $password = '';
-  $database = 'hers_TD';
+  $password = 'root';
+  $database = 'GD2ATD';
   //TODO: Player name?
   //TODO: sanitize input
-
   //if no score is defined, code won't work
   if($score != ""){
     //connect with database (other user/password combinations may be needed)
-    $conn = mysqli_connect($host, $user, $password, $database) or die('Failed to connect: ' . mysqli_error());
-    
-    $query="INSERT INTO HighScores(id, score, name) VALUES(DEFAULT, '$score', Silvan);";
+    $conn = mysqli_connect($host, $user, $password, $database) or die('Failed to connect: ' . mysqli_error($conn));
+    $query="INSERT INTO Scores(id, score, player_name, time) VALUES(DEFAULT, '$score', 'Silvan', DEFAULT);";
     $result=mysqli_query($conn, $query);
-
     if ($result) {
       echo "New record created successfully";
     } else {
         echo "Error: " . $query . "<br>" . mysqli_error($conn);
     }
-
   } else{
     echo "Score was empty";
   }
-
  ?>
