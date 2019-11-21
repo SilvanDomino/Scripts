8866f5f11da2e6da946739dd5ced0e0ef507149a
Silvan Herrema <silvah489@gmail.com>
2019-11-21 09:58:37 +0100

    Add files via upload

---
+++ MoveScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    public float speed;
+
+    void Start()
+    {
+        Debug.Log("Hello World");
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + new Vector3(speed, 0, 0);
+        }
+        
+    }
+}
