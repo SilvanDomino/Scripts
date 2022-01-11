335ff61ae1d7b16bdc3c14cb1a68dd34394a9054
SilvanDomino <silvah489@gmail.com>
2022-01-11 08:09:56 +0100

    Added cats.js

---
+++ JS/cats.js
@@ -0,0 +1,24 @@
+const cat1 = {
+    naam: "Kitty",
+    geslacht: "V",
+    leeftijd: 1,
+    afbeelding: "https://placekitten.com/400/300",
+}
+const cat2 = {
+    naam: "Boris",
+    geslacht: "M",
+    leeftijd: 2,
+    afbeelding: "https://placekitten.com/401/300",
+}
+const cat3 = {
+    naam: "Dexter",
+    geslacht: "M",
+    leeftijd: 7,
+    afbeelding: "https://placekitten.com/402/300",
+}
+const cat4 = {
+    naam: "Neeltje",
+    geslacht: "V",
+    leeftijd: 6,
+    afbeelding: "https://placekitten.com/403/300",
+}
